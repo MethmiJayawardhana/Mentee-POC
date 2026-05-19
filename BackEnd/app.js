1ca214cac0c4ddc58c07f65c195bbec808e14626
Methmi Jayawardhana <methmisdj@gmail.com>
2026-05-20 02:44:36 +0530

    add some of the files to .gitignore and remove them from git tracking

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -12,11 +12,10 @@ app.use((req, res, next) => {
   next();
 });
 
-// CORS headers
+// Basic CORS for local development
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
